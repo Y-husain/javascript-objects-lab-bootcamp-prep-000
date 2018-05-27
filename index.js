1481305fb63a9d15ca1155ea90f611189148a7f8
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 12:08:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function updateObjectWithKeyAndValue(obj,key,value){
   return Object.assign({},obj,{[key]:value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj,key,value)
-{
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+  //change obj argument by adding new key value pair
+  obj[key] = value
   return obj
 }
