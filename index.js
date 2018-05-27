9cddecc9da18d3b26b3b8aba9c905b3b0d6ce47a
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 12:09:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ function destructivelyUpdateObjectWithKeyAndValue(obj,key,value)
   
   return obj
 }
+
+function deleteFromObjectByKey(obj,key)
+{
+  //create a duplicate of obj so that obj can remain unchanged
+  
+  var obj1 = Object.assign({},obj)
+  delete obj1[key]
+  return obj1
+}
