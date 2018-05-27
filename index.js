e8e56157dc6f34de5de8cedb51220c5fb67b8acb
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 12:06:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-var recipes = {bananas:1}
+var recipes = {bananas:200}
 
+function updateObjectWithKeyAndValue(obj,key,value){
+  //return updated object without changing obj in arguments
+  return Object.assign({},obj,{[key]:value})
+}
