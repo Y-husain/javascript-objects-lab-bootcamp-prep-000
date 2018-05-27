ba0832c547b6ebf07a58c99bfa37a10e986f49e1
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 12:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {bananas:200}
 function updateObjectWithKeyAndValue(obj,key,value){
   return Object.assign({},obj,{[key]:value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+
+  return obj
+}
